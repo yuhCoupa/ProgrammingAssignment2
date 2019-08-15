3862f4fc955b2d3c56f15023c74d2efd516cc97c
Hongyang Yu <43979743+yuhCoupa@users.noreply.github.com>
2019-08-15 10:10:21 +1000

    added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,29 @@
+# clear global env
 rm(list=ls())
 
+# this function calculates the inverse of an invertible matrix
 makeCacheMatrix <- function(x = diag(2)) {
   invX <- NULL
+  # this enclosed function caches the input matrix for comparison later
   set <- function(y){
     cachedX <<- y
     invX <<- NULL
   }
   
+  # get the input matrix
   get <- function() x
+  # get the cached matrix from previous input
   getCachedX <- function() cachedX
+  # cache the inverted matrix
   setInvX <- function(inverseX) invX <<- inverseX
+  # get the inverted matrix
   getInvX <- function() invX
   list(set = set, get = get,
        setInvX = setInvX,
        getInvX = getInvX)
 }
 
-
+# this function gets the inverted matrix
 cacheSolve <- function(x) {
   invX <- x$getInvX()
   # if invX is null, set the cached matrix equal to the input matrix and calculate invX
